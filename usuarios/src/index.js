3bb3f1ba5a6be1c281a4480ed23b9fd0210dd2a7
JOAO VICTOR SANTOS DE OLIVEIRA <joao.vsoliveira65@senacsp.edu.br>
2024-06-20 13:58:45 -0300

    Feature: adição de novos recurso

--- usuarios/src/index.js
+++ usuarios/src/index.js
@@ -1,3 +1,17 @@
 require("dotenv").config();
 
-console.log(process.env.JWT_KEY)
+const express = require("express");
+const router = require("./routes/users/users.js")
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/v1/users/",router);
+
+app.listen(process.env.HOST_PORT,()=>{
+    console.log(`Servidor online em: 
+    ${process.env.HOST_NAME}:
+    ${process.env.HOST_PORT}`)
+}
+);
+
